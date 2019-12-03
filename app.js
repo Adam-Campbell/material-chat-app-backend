3769aa2298bc45b026f86620f78e1a67ae9370c6
Adam <adamcampbelldev@gmail.com>
2019-12-03 23:34:37 +0000

    Began moving functionality over to socketio

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createServer } = require('http');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const session = require('express-session');
@@ -6,6 +7,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongoSessionStore = require('connect-mongo');
 const MongoStore = mongoSessionStore(session);
+const socketIO = require('socket.io');
+const socketioJwt = require('socketio-jwt');
+const {
+    getCurrentUsersConversations,
+    getConversation,
+    sendMessage
+} = require('./socketHandlers');
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +23,47 @@ const meRouter = require('./routes/me');
 const usersRouter = require('./routes/users');
 
 const app = express();
+const server = createServer(app);
+const io = socketIO(server);
+
+const currentUsers = new Map();
+
+io.sockets.on('connection', socketioJwt.authorize({
+    secret: 'foobar',
+    timeout: 15000
+}))
+.on('authenticated', socket => {
+    currentUsers.set(socket.decoded_token._id, socket.id);
+    console.log(`This is the socket for user with id ${socket.decoded_token._id}`);
+})
+
+
+io.on('connection', socket => {
+    console.log('A socket connected');
+    //console.log(socket.id);
+    //console.log(io.sockets);
+    
+    socket.on('disconnect', () => {
+        console.log('A socket disconnected')
+        currentUsers.delete(socket.decoded_token._id);
+    });
+
+    socket.on('foo', data => {
+        console.log(data);
+        console.log(`From inside foo ${socket.decoded_token._id}`);
+        socket.emit('baz', 'This is baz');
+    });
+
+    socket.on('getConversations', () => {
+        getCurrentUsersConversations(socket);
+    });
+    socket.on('getConversation', conversationId => {
+        getConversation(socket, conversationId)
+    });
+    socket.on('sendMessage', (conversationId, messageText) => {
+        sendMessage(socket, conversationId, messageText, currentUsers);
+    });
+})
 
 mongoose.connect(
     'mongodb://localhost/materialChatApp',
@@ -36,9 +85,11 @@ app.use(session({
     //store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
+//app.use(sessionInstance);
+
 app.use('/auth', authRouter);
 app.use('/conversations', conversationsRouter);
 app.use('/me', meRouter);
 app.use('/users', usersRouter);
 
-app.listen(port, () => console.log(`App is running on port ${port}`));
+server.listen(port, () => console.log(`App is running on port ${port}`));
