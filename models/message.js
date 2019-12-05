1c5cd49f7cd85e88a6a01d49de146da21be154d5
Adam <adamcampbelldev@gmail.com>
2019-12-05 18:01:19 +0000

    Added timestamps to message and conversation models

--- models/message.js
+++ models/message.js
@@ -10,6 +10,10 @@ const MessageSchema = new Schema({
     body: {
         type: String,
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
 
